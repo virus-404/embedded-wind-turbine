ccafd711d15f768961fd6e0333fbfee3de0717a4
edferoca <47976279+edferoca@users.noreply.github.com>
2021-11-12 15:38:02 +0100

    adicion de pruebas para implementacion de lcd

---
+++ Data_Producer_1/test/LCD_ESP01_1.0/LCD_ESP01_1.0.ino
@@ -0,0 +1,35 @@
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include "SerialLCDesp01.h"
+
+SoftwareSerial softSerial(2, 3); // RX, TX
+LCD LCD;
+
+void setup() {
+  Serial.begin(115200);
+  softSerial.begin(115200);
+  LCD.setHome();//set the cursor back to 0,0.
+  LCD.clearScreen();//clear anything that may have been previously printed ot the screen.
+  delay(10);
+  
+  LCD.printStr("Commence Arduino Demo Mode");
+  delay(1500);
+
+  
+}
+void loop() {
+  LCD.clearScreen();
+  LCD.printStr("te amo aleja");
+  delay(2000);
+  LCD.clearScreen();
+  
+  
+}  
+
+void clearScreen()
+{
+  //clears the screen, you will use this a lot!
+  Serial.write(0x7C);
+  Serial.write((byte)0); //CTRL @
+  //can't send LCD.write(0) or LCD.write(0x00) because it's interprestted as a NULL
+}
