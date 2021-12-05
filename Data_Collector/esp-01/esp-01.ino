31fc71e55b28a2a94ff796c527761b3d123847fa
AGAPITO GALLART BERNAT <agapito1998@gmail.com>
2021-12-05 12:41:54 +0100

    Creation of files + SP-01 [1/2]

---
+++ Data_Collector/esp-01/esp-01.ino
@@ -0,0 +1,65 @@
+#include "wifi_config.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  client.connect(clientID,clientUserName,clientPassword);
+  client.subscribe("broker/counter");
+}
+
+void loop() {
+
+  client.loop();
+  delay(10);
+  //Serial.println("A");
+}
---
+++ Data_Collector/esp-01/wifi_config.h
@@ -0,0 +1,10 @@
+/*
+ * Your WiFi config here
+ */
+char ssid[] = "wind_tutut";      // your network SSID (name)
+char pass[] = "edferoca"; // your network password
+const char* mqtt_server = "192.168.4.1";
+
+const char* clientID = "ESP-01";
+const char* clientUserName = "ESP-01";
+const char* clientPassword = "ESP-01";
