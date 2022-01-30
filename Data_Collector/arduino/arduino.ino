c5911dd2ae458ce367fec112f510f9755f99407f
edferoca <47976279+edferoca@users.noreply.github.com>
2022-01-30 16:31:09 +0100

    arduino

--- Data_Collector/arduino/arduino.ino
+++ Data_Collector/arduino/arduino.ino
@@ -10,7 +10,8 @@ SoftwareSerial softSerial(2, 3); // RX, TX
 
 
 int leng=0;
-char cola[25];
+char cola[29];
+char pos[14];
 uint8_t sp01;
 
 void receiveFunc()
@@ -19,9 +20,15 @@ void receiveFunc()
 }
 
 void sendFunc()
-{
-  Wire.write(cola[leng]); 
-  Serial.println(cola[leng]);
+{ 
+  for(int i = 0;i<=29;i++){
+    Wire.write(cola[i]); 
+    Serial.print(cola[i]);
+  }
+  Serial.println();
+  //Wire.write(cola[leng]); 
+  //Serial.println(cola[leng]);
+  
 }
 
 
@@ -39,11 +46,48 @@ void setup()
 
 void loop()
 {
+  if(Serial.available()){
+    cola[leng]=Serial.read();
+    //Serial.print(cola[leng]);
+    leng=leng+1;
+    if(leng==30 || cola[leng-1]=='p' ){
+      leng=0; 
+    }
+  }
+  //Serial.print(cola[29]);
+  int p=0;
+  for(int i=0; i<=31;i++){   
+    if(cola[i]=='p'){
+      for(int q=0;i<=31;i++){
+        pos[q]=cola[i];
+        i=i+1;
+      }
+
+      
+      
+      //Serial.print(pos[p]);
+      p=p+1;
+      if(p==15){
+        p=0; 
+      }
+    }
+    //fin de la matris de pos
+    
+   }
  
+      
+    
+
+  }
+  //Serial.println();
+
+  
+
+ /*
   while(!Serial.available());
     cola[leng]= Serial.read();  
     leng=leng+1;
     if(leng==25){
       leng=0; 
-    }  
-}
+    }
+    */  
